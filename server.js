26144a45e99b56be3130e14a6841621a30038208
deneuvillem <maxime.deneuville@student.uclouvain.be>
2020-07-09 21:37:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-const { count } = require('console');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
